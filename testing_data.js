82c342b15d9191f9d4b8a4bc3d5f7955e1360453
Eva Sarafianou <eva.sarafianou@gmail.com>
2018-04-02 14:12:00 -0700

    improve testing data

--- testing_data.js
+++ testing_data.js
@@ -1,26 +1,20 @@
 const { Sequelize } = require('sequelize')
-const { Client } = require('./database.js')
+const { Client, syncPromise } = require('./database.js')
 
 const database = process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/postgres'
 
-const Connection = new Sequelize(
-  database,
-  { operatorsAliases: false }
-)
 
-Connection.sync().then(() => {
+syncPromise.then(() => {
   return Client.create({
     name: 'clientApp',
     client_id: '12345678',
     client_secret: 'za98df76fv45bg90bg12',
-    redirect_uri:'https://evasar.io'
+    redirect_uri:'https://auth0.com'
   })
   .then(() => {
     console.log('client created')
-    Connection.close()
   })
   .catch((error) => {
-    Connection.close()
     console.log(error)
   })
 })
